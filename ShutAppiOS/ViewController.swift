1540b6282283efd2ec5649235561cc017b970399
Erik Ugarte <erik-nu@hotmail.com>
2020-12-16 11:33:21 +0100

    Initial Commit

---
+++ ShutAppiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShutAppiOS
+//
+//  Created by Erik Ugarte on 2020-12-16.
+//  Copyright © 2020 ShutApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
